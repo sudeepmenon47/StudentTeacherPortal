814bc552f558d08b7082a9771e355af0f2749d02
balaji029 <balajijo97@gmail.com>
2022-04-15 14:57:27 +0100

    Exam_view structure

---
+++ application/views/exam/view_exam_view.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>View Exam</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+</body>
+</html>
